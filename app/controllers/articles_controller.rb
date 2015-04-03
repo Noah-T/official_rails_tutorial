fbd7f69c5cf04d9967d10f9f8d74e8d81d80afc6
Noah Teshu <nteshu@gmail.com>
2015-04-03 17:45:18 -0400

    added basic creation and display of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,29 @@
 class ArticlesController < ApplicationController
+	def index
+		#return all articles
+		@articles = Article.all
+	end
+
 	def new
 	end
 
 	def create
-		render plain: params[:article].inspect
+		#instance variable @article becomes a new article which is passed :article as a param
+		#permit means these properties can be written to
+		@article = Article.new(article_params)
+
+		@article.save
+
+		redirect_to @article
 	end
+
+	def show
+		#:id will give us the id associated with the request
+		@article = Article.find(params[:id])
+	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end	
 end
